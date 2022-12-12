1b480ece373ae46b363336f94966d2ccdd6c89b0
Dillon Shaver <dshaver@anl.gov>
2022-12-12 15:52:15 -0600

    make doall actually do all

--- doall.sh
+++ doall.sh
@@ -1,10 +1,10 @@
 [ -e converter ] && rm converter
 
-#cd matlab
-#matlab -nodisplay -r wire_mesher
-#mv *.out ../
-#cd ..
-#python bintoascii.py
+cd matlab
+matlab -nodisplay -r wire_mesher
+mv *.out ../
+cd ..
+python bintoascii.py
 cd wire2nek
 ./compile_wire2nek
 cp wire2nek ../converter
